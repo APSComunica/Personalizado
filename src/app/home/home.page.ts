ac7a54610915b2dec1087d2bf8ea1643955799f2
APSComunica <andresperezsanchez12314@gmail.com>
2020-11-18 09:27:42 +0100

    uno

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,7 +12,7 @@ export class HomePage {
   users: any;
 
   constructor(private httpClient: HttpClient) {
-    this.users = this.httpClient.get('https://randomuser.me/api/?results=20').pipe(map(res => res['results']));
+    this.users = this.httpClient.get('https://github.com/APSComunica/Personalizado/blob/master/src/assets/data/MOCK_DATA.json').pipe(map(res => res['results']));
   }
 
   
